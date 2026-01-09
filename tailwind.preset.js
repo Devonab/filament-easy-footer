2b5bd797b55f415f81afcd2d22845050b125afdf
Alexandre <6360488+Devonab@users.noreply.github.com>
2026-01-09 19:56:46 +0100

    Update Tailwind config and dependencies, adjust CSS
    Upgraded Tailwind and related dependencies, added tailwind.preset.js, and updated CSS to apply min-height and height styles to .fi-main.

---
+++ tailwind.preset.js
@@ -0,0 +1,10 @@
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+
+export default {
+    darkMode: 'class',
+    plugins: [
+        forms,
+        typography,
+    ],
+}
